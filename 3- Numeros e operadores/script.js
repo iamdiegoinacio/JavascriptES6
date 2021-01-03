547edfabc655d8a617bfb209e35842f204f3eba7
Diego Inácio <62408076+iamdiegoinacio@users.noreply.github.com>
2021-01-03 18:54:21 -0200

    Números e operadores I

---
+++ 3- Numeros e operadores/script.js
@@ -0,0 +1,14 @@
+let soma = 10 + 10;
+let sub = 100 - 50;
+let multi = 10 * 2;
+let divisao = 100 / 2;
+let expo = 2 ** 4;
+
+
+console.log(soma, sub, multi, divisao, expo);
+
+if(isNaN(soma)){
+    console.log("É NaN");
+}else{
+    console.log("Não é NaN");
+}
