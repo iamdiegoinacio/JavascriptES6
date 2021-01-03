fa745e8e3941e644da66895cc62d84b52da74cd2
iamdiegoinacio <iamdiegoinacio@gmail.com>
2021-01-03 13:30:13 -0200

    Criação da pasta tipos de dados e organização da mesma

--- 1-Variáveis/script.js
+++ 1-Variáveis/script.js
@@ -1,11 +1,7 @@
 let nome = "Diego"; //String
 let idade = 25; //Number
 let peso = 84.5; //Number
-let masculino = true; //Boolean
-let time; //Undefined
-let comida = null; //Null
-let simbolo = Symbol() //Symbol
-let novoObjeto = {} //Object
+let masculino = true;
 
 
 console.log(nome, idade, peso, masculino);
