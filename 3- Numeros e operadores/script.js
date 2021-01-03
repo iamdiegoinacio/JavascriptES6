97168255d4cf6c33e33475ded2990ca44deb2859
Diego Inácio <62408076+iamdiegoinacio@users.noreply.github.com>
2021-01-03 19:02:44 -0200

    Pré e pós incremento

--- 3- Numeros e operadores/script.js
+++ 3- Numeros e operadores/script.js
@@ -12,3 +12,22 @@ if(isNaN(soma)){
 }else{
     console.log("Não é NaN");
 }
+
+
+// Pré e pós Incremento 
+var i = 1;
+
+/* Ao fazer o incremento usando "i++", ocorre da saída ser mostrada
+primeiro do que o incremento, então mesmo que o primeiro console.log 
+há um incremento, a saída será 1, e somente depois no segundo console.log
+que a saída será 2.*/
+
+console.log(i++);
+console.log(i);
+
+/*Ao fazer o incremento usando "++i", ocorre do incremento ser feito antes
+da variável i ser mostrada, por conta disso, na primeira chamada a var i
+já é mostrada como 2 (ao contrário da primeira forma de incremento)*/
+
+i = 1;
+console.log(++i);
