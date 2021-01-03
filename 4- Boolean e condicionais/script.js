2f3bd21d82d131a240273f8014d8bc0f60e517ed
Diego Inácio <62408076+iamdiegoinacio@users.noreply.github.com>
2021-01-03 19:20:56 -0200

    Criação da pasta booolean e condicionais e seus respectivos arquivos

---
+++ 4- Boolean e condicionais/script.js
@@ -0,0 +1,29 @@
+/* 
+
+Truthy e Falsy 
+
+Existem valores que retornam true e outros que retornam false quando verificamos em uma 
+expressão booleana.
+
+*/
+
+//Falsy
+
+/*
+
+false
+
+0
+
+NaN
+
+null
+
+undefined
+
+'' ou "" ou ``
+
+Os demais são Truthy
+
+*/
+
